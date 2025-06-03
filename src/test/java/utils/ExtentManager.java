718954f4694181c75ae7c3eff9d30298d5db3d19
emreguduk <emre.guduk@testinium.com>
2025-06-03 16:11:02 +0300

    fixed

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -2,6 +2,7 @@ package utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
 
@@ -16,8 +17,10 @@ public class ExtentManager {
 
     private static ExtentReports createInstance() {
         ExtentSparkReporter reporter = new ExtentSparkReporter("test-output/ExtentReport.html");
-        reporter.config().setReportName("Automation Test Report");
-        reporter.config().setDocumentTitle("Test Results");
+        reporter.config().setEncoding("UTF-8");
+        reporter.config().setReportName("Insider Test Automation Report");
+        reporter.config().setDocumentTitle("Automation Execution Summary - Insider");
+        reporter.config().setTheme(Theme.STANDARD);
 
         ExtentReports extent = new ExtentReports();
         extent.attachReporter(reporter);
